22739e47e444028eedb4ba81e8c75ab06fb85dfa
Nayanne Batista <nayannelopes.bd@gmail.com>
2024-02-02 17:46:23 -0300

    commit inicial

---
+++ src/app/services/contato.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { Contato } from '../componentes/contato/contato';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ContatoService {
+
+  private contatos: Contato[] = [
+    {"id": 1, "nome": "Ana", "telefone": "29 278869420", "email": "email@emal.com"},
+  ]
+
+  constructor() {
+    //Tentar obter os dados do localStorage
+    const contatosLocalStorageString = localStorage.getItem('contatos');
+    const contatosLocalStorage =
+      contatosLocalStorageString ? JSON.parse(contatosLocalStorageString) : null;
+
+    this.contatos = contatosLocalStorage || null;
+
+    //Salvar os contatos no localStorage
+    localStorage.setItem('contatos', JSON.stringify(this.contatos));
+  }
+
+  obterContatos() {
+    return this.contatos;
+  }
+
+  salvarContato(contato: Contato) {
+    this.contatos.push(contato);
+    localStorage.setItem('contatos', JSON.stringify(this.contatos));
+  }
+}
